ce831570a23f9ca3c1d37d15fde0bcfb12e9c64d
aruna <arunakamakshi09@gmail.com>
2025-03-17 18:01:23 +0530

    Updated the about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
     const [activeTab, setActiveTab] = useState<'professional' | 'education' | 'personal'>('professional');
 
     return (
-        <div className="max-w-4xl mx-auto px-4 py-12">
-            <h1 className="text-4xl font-bold mb-8 text-gray-800">About Me</h1>
+        <div className="max-w-4xl mx-auto px-4 py-12 mt-20">
+            <h1 className="text-3xl  mb-8 text-gray-800">About Me</h1>
 
             {/* Tab Navigation */}
             <div className="flex mb-8 border-b">
