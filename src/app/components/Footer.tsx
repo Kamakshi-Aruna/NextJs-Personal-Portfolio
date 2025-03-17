392c6aa1712b4dc520fbdefa12bbc008642700d1
aruna <arunakamakshi09@gmail.com>
2025-03-17 18:54:59 +0530

    Updated the footer page

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
             <div className="max-w-7xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
 
                 {/* Copyright Text */}
-                <p className="text-sm">&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+                <p className="text-sm">&copy; {new Date().getFullYear()} Aruna Kamakshi. All rights reserved.</p>
 
                 {/* Social Links */}
                 <div className="flex space-x-4 mt-3 md:mt-0">
